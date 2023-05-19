2fd561c38c1f15dfe268ec6222e5474a9e07ee3b
Denis <dambatenne@gmail.com>
2023-05-19 16:47:55 +0200

    Changes in build script

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -10,6 +10,18 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
 tasks.withType<KotlinCompile>().configureEach {
     compilerOptions {
         languageVersion.set(KotlinVersion.KOTLIN_1_5)
